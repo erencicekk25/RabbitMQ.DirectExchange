efa343f4f0555055c362a2cad51dc5b49434c7f7
Eren Çiçek <117859063+erencicekk25@users.noreply.github.com>
2023-11-19 02:22:20 +0300

    directExchange

---
+++ RabbitMQ.DirectExchange/RabbitMQ.DirectExchange.Consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://wylxhthv:9gEUm_Hi-PGavdDsheGhvL106sliPAi5@shark.rmq.cloudamqp.com/wylxhthv ");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 1, false);
+var consumer = new EventingBasicConsumer(channel);
+
+var queueName = "direct-queue-Critical";
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Loglar dinleniyor...");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+    Console.WriteLine($"Gelen mesaj: {message}");
+
+    File.AppendAllText("log-critical.txt", message + "\n");
+    channel.BasicAck(e.DeliveryTag, false);
+};
+Console.ReadLine();
+
+//Eren Çiçek
+//Bu proje serisi RabbitMQ mesaj kuyruk sistemini daha iyi anlamak için yapılmıştır.
---
+++ RabbitMQ.DirectExchange/RabbitMQ.DirectExchange.Publisher/Program.cs
@@ -0,0 +1,45 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://wylxhthv:9gEUm_Hi-PGavdDsheGhvL106sliPAi5@shark.rmq.cloudamqp.com/wylxhthv ");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+
+Enum.GetNames(typeof(LogNames)).ToList().ForEach(x =>
+{
+    var routeKey = $"route-{x}";
+    var queueName = $"direct-queue-{x}";
+
+    channel.QueueDeclare(queueName, true, false, false);
+    channel.QueueBind(queueName, "logs-direct", routeKey, null);
+});
+
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    LogNames log = (LogNames)new Random().Next(1, 5);
+    string message = $"log-type: {log}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    var routeKey = $"route-{log}";
+
+    channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+    Console.WriteLine($"Log gönderildi: {message}");
+});
+
+
+public enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Info = 4
+}
+
+
+//Eren Çiçek
+//Bu proje serisi RabbitMQ mesaj kuyruk sistemini daha iyi anlamak için yapılmıştır
